71fb21f3c7fcfc66de8253e9558122dbc24cbef6
suhail810 <pathansuhail810@gmail.com>
2024-07-18 23:44:43 +0530

    work on store folder like, authSlice, create reducers

---
+++ src/store/authSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    status: false,
+    userData: null
+}
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: { }
+})
+
+export default authSlice.reducer
